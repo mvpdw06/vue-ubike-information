853ddf61260713e32c411799932bc763f88e842c
RyanHsu <mvpdw06@gmail.com>
2017-08-21 23:55:48 +0800

    implement webpack bundle and webpack dev-server for development. Using Babel to translate ES6+ JavaScript. Including Vue and axios by node_modules. Use prettier to format my code.

---
+++ webpack/devServer.js
@@ -0,0 +1,23 @@
+import path from "path";
+import webpack from "webpack";
+import webpackDevServer from "webpack-dev-server";
+import config from "./webpack.config.babel";
+
+const HOST = "localhost";
+const PORT = 9000;
+
+const server = new webpackDevServer(webpack(config), {
+  contentBase: path.join(__dirname, "../output"),
+  publicPath: config.output.publicPath,
+  historyApiFallback: true,
+  hot: true,
+  stats: {
+    chunks: false,
+    colors: true
+  }
+});
+
+server.listen(PORT, HOST, err => {
+  if (err) console.log(`Error: ${err}`);
+  else console.log(`Server is running on ${HOST}:${PORT}`);
+});
---
+++ webpack/webpack.config.babel.js
@@ -0,0 +1,79 @@
+import path from 'path';
+import webpack from 'webpack';
+
+const env = process.env.NODE_ENV || 'development';
+const isDevEnv = env === 'development';
+
+// webpack settings
+const getDevtoolSetting = () => {
+    return isDevEnv ?
+        'cheap-module-eval-source-map' :
+        'cheap-module-source-map';
+}
+const getPluginsSetting = () => {
+    const plugins = [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(env)
+        })
+    ];
+    if (isDevEnv) {
+        plugins.push(
+            new webpack.HotModuleReplacementPlugin()
+        );
+    } else {
+        plugins.push(
+            new webpack.optimize.UglifyJsPlugin({
+                compress: {
+                    warnings: false,
+                    drop_console: false,
+                }
+            })
+        );
+    }
+    return plugins;
+}
+const getEntrySetting = () => {
+    const entry = [
+        path.join(__dirname, '../src/index')
+    ];
+    if (isDevEnv) {
+        entry.push.apply(
+            entry,
+            [
+                'webpack/hot/only-dev-server',
+                'webpack-dev-server/client?http://localhost:9000'
+            ]
+        );
+    }
+    return entry;
+}
+
+const webpackConfig = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: [/node_modules/]
+            }
+        ]
+    },
+    devtool: getDevtoolSetting(),
+    plugins: getPluginsSetting(),
+    entry: getEntrySetting(),
+    resolve: {
+      alias: {
+        vue: 'vue/dist/vue.js'
+      }
+    }
+}
+
+const AppConfig = Object.assign({}, webpackConfig, {
+    output: {
+        path: path.join(__dirname, '../output/assets'),
+        filename: 'app.js',
+        publicPath: '/assets'
+    }
+});
+
+module.exports = AppConfig;
